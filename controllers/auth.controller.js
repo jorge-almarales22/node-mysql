294f969ae2a7b9d76a7d9a6c81b3543f49bad0f2
Jorge Almarales <jorgealmarales22@gmail.com>
2023-09-13 22:43:02 -0500

    JWT added to the sign out

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,21 @@
+import { pool } from "../database/config.js"
+import { generateJWT } from "../helpers/generateJWT.js"
 import { googleVerify } from "../helpers/google-verify-token.js"
 
 export const loginWithGoogle = async(req, res) => {
 
     const {token} = req.body
 
-    const {email, name, picture} = await googleVerify(token)
+    const {email, name, picture: image} = await googleVerify(token)
 
-    res.json({email, name, picture})
+
+    const [resp] = await pool.query('insert into users (name, salary, image, email, password) values (?, ?, ?, ?, ?)', [name, 1234, image, email, 'password'])
+
+    if(resp.affectedRows == 0){
+        return res.status(400).json({msg: 'User not found'})
+    }
+
+    const tkn = await generateJWT(resp.insertId)
+
+    res.json({ok: true, message: "usuario logueado", token: tkn, email })
 }
